6febd123d72a9f47a04c45bb135750ae35647e50
HectorGerardoZV <torrozapata@gmail.com>
2022-12-17 21:23:46 -0700

    App configuration with .env was finished

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,18 @@ import { ValidationPipe } from '@nestjs/common/pipes';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { TransformInterceptor } from "./transform.interceptor";
+import { Logger } from "@nestjs/common";
+
 
 async function bootstrap() {
+  const logger = new Logger();
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
-  await app.listen(3000);
+  const port: string = '3000';
+  const host: string = 'localhost';
+  await app.listen(port, host, () => {
+    logger.log(`Server running in ${host}:${port}`);
+  });
 }
 bootstrap();
